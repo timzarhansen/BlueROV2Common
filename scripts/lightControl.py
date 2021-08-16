d36409706c6242f6c6c48f940a51ab0b2a866e18
tim-linux <timhansen93@googlemail.com>
2021-08-16 12:12:13 +0200

    update leakage sensor

--- scripts/lightControl.py
+++ scripts/lightControl.py
@@ -9,15 +9,15 @@ try:
     while 1:
         print("starting Light")
         p.ChangeDutyCycle(1)
-        time.sleep(0.5)
+        time.sleep(1.5)
         p.ChangeDutyCycle(3)
-        time.sleep(0.5)
+        time.sleep(1.5)
         p.ChangeDutyCycle(6)
-        time.sleep(0.5)
+        time.sleep(1.5)
         p.ChangeDutyCycle(8)
-        time.sleep(0.5)
+        time.sleep(1.5)
         p.ChangeDutyCycle(10)
-        time.sleep(0.5)
+        time.sleep(1.5)
 except KeyboardInterrupt:
     pass
     p.stop()
