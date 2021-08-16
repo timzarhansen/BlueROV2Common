c843397b5ddad8b8e17801d06c35489462277868
tim-linux <timhansen93@googlemail.com>
2021-08-16 12:46:08 +0200

    update leakage sensor

---
+++ scripts/cameraServoControl.py
@@ -0,0 +1,27 @@
+import time
+import RPi.GPIO as GPIO
+
+
+
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.OUT)
+
+p = GPIO.PWM(12, 50)  # frequency=50Hz
+p.start(0)
+
+
+
+
+try:
+    while 1:
+        for i in range(0,100,2):
+            p.ChangeDutyCycle(i)
+            print(i)
+            time.sleep(2)
+
+except KeyboardInterrupt:
+    pass
+    p.stop()
+    GPIO.cleanup()
