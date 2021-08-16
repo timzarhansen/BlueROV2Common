1f8d7f8d0fdadfa76617deb416f21d90f9aec47c
tim-linux <timhansen93@googlemail.com>
2021-08-16 12:08:44 +0200

    update leakage sensor

--- scripts/lightControl.py
+++ scripts/lightControl.py
@@ -7,9 +7,16 @@ p = GPIO.PWM(23, 50)  # frequency=50Hz
 p.start(0)
 try:
     while 1:
-        for dc in range(0, 101, 5):
-            p.ChangeDutyCycle(dc)
-            time.sleep(0.1)
+        p.ChangeDutyCycle(1)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(2)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(3)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(4)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
 except KeyboardInterrupt:
     pass
     p.stop()
