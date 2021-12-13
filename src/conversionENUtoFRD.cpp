2bc0fc3fc3495685c241fc8e966ed2e7ac85739e
tim-linux <t.hansen@jacobs-university.de>
2021-12-13 13:46:05 +0100

    hopefully correct ENU to FRD change

--- src/conversionENUtoFRD.cpp
+++ src/conversionENUtoFRD.cpp
@@ -8,6 +8,8 @@
 #include "geometry_msgs/TwistStamped.h"
 #include <Eigen/Geometry>
 #include <Eigen/StdVector>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 class rosConversionClass {
 public:
@@ -46,7 +48,26 @@ private:
         newMsg.linear_acceleration.x = acceleration.x();
         newMsg.linear_acceleration.y = acceleration.y();
         newMsg.linear_acceleration.z = acceleration.z();
-        newMsg.orientation = msg->orientation;//not sure if correct
+        Eigen::Quaterniond rotationRP(msg->orientation.w,msg->orientation.x,msg->orientation.y,msg->orientation.z);
+
+        Eigen::Vector3d rollPitchYaw = getRollPitchYaw(rotationRP.inverse());
+        // @TODO create low pass filter
+        double rollIMUACCEL = atan2(-msg->linear_acceleration.y,msg->linear_acceleration.z);
+        double pitchIMUACCEL = atan2(msg->linear_acceleration.x,sqrt(msg->linear_acceleration.y*msg->linear_acceleration.y+msg->linear_acceleration.z*msg->linear_acceleration.z));
+//        std::cout << "my Roll: "<< rollIMUACCEL*180/M_PI<< std::endl;
+//        std::cout << "my Pitch: "<< pitchIMUACCEL*180/M_PI<< std::endl;
+
+        //std::cout <<"r: " <<rollPitchYaw(0)*180/M_PI <<" p: " << rollPitchYaw (1)*180/M_PI<< " y: " <<rollPitchYaw(2)*180/M_PI << std::endl;
+        //rollPitchYaw(1)=-rollPitchYaw(1);//only invert pitch
+//        if(abs(rollPitchYaw(0))<M_PI/2){
+//            rollPitchYaw(1)=-rollPitchYaw(1);
+//        }
+        rotationRP = getQuaternionFromRPY(-rollIMUACCEL,pitchIMUACCEL,0);
+        newMsg.orientation.x = rotationRP.x();//not sure if correct
+        newMsg.orientation.y = rotationRP.y();//not sure if correct
+        newMsg.orientation.z = rotationRP.z();//not sure if correct
+        newMsg.orientation.w = rotationRP.w();//not sure if correct
+
         publisherImuData.publish(newMsg);
 
     }
@@ -79,6 +100,33 @@ private:
         newMsg.relative=-msg->relative;
         newMsg.amsl=-msg->amsl;
         publisherAltitude.publish(newMsg);
+    }
+
+    Eigen::Vector3d getRollPitchYaw(Eigen::Quaterniond quat){
+        tf2::Quaternion tmp(quat.x(),quat.y(),quat.z(),quat.w());
+        tf2::Matrix3x3 m(tmp);
+        double r, p, y;
+        m.getRPY(r, p, y);
+        Eigen::Vector3d returnVector(r,p,y);
+        return returnVector;
+    }
+
+    Eigen::Quaterniond getQuaternionFromRPY(double roll, double pitch, double yaw){
+//        tf2::Matrix3x3 m;
+//        m.setRPY(roll,pitch,yaw);
+//        Eigen::Matrix3d m2;
+        tf2::Quaternion qtf2;
+        qtf2.setRPY(roll,pitch,yaw);
+        Eigen::Quaterniond q;
+        q.x()=qtf2.x();
+        q.y()=qtf2.y();
+        q.z()=qtf2.z();
+        q.w()=qtf2.w();
+
+//        q = Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX())
+//            * Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY())
+//            * Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
+        return q;
     }
 };
 
