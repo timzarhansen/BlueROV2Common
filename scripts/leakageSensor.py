fdd22b1dffcf07e1875aa26d781217f176bd62fa
BogdanBelenis <bogdanbelenis@gmail.com>
2023-10-10 14:47:14 +0200

    Transitioned leak sensor and lights to ROS2.

--- scripts/leakageSensor.py
+++ scripts/leakageSensor.py
@@ -1,42 +1,58 @@
 #!/usr/bin/env python3
-
-from time import sleep           # Allows us to call the sleep function to slow down our loop test
-from commonbluerovmsg.msg import leakageDetection
+import sys
+from commonbluerovmsg.msg import LeakageDetection
 import RPi.GPIO as GPIO           # Allows us to call our GPIO pins and names it just GPIO
-import rospy
+import rclpy
+from rclpy.node import Node
 
 
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
+
+class LeakPublisher(Node):
+
+    def __int__(self):
+        super().__init__('leakage_status')
+        self.publisher_ = self.create_publisher(LeakageDetection, 'leakage_status', 1)
+        # executes callback every timer_period seconds (change to appropriate value)
+        timer_period = 1
+        self.timer = self.create_timer(timer_period, self.leak_callback)
+        self.count = 0
+
+    def leak_callback(self):
+        if GPIO.input(INPUT_PIN):  # Physically read the pin now
+            print(colored(255, 0, 0, '################# MAYDAY LEAKAGE DETECTED WATER IN THE BOAT #################'))
+            msg = LeakageDetection()
+            msg.timestamp = self.get_clock().now().to_msg()
+            msg.leakage_detected = True
+            self.publisher_.publish(msg)
+            self.count += 1
+        else:
+            msg = LeakageDetection()
+            msg.timestamp = self.get_clock().now().to_msg()
+            msg.leakage_detected = False
+            self.publisher_.publish(msg)
+            self.count += 1
+
+
 def main():
+    rclpy.init(args=sys.argv)
+    leak_publisher = LeakPublisher()
 
-    rospy.init_node('drive_scenario_01')
-    rate = rospy.Rate(1)
-    publisher_leakageStatus = rospy.Publisher('leakageStatus', leakageDetection, queue_size=1)
-
-
+    # Init GPIO pins -> not affected by ROS2 migration
     try:
-        GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
-        INPUT_PIN = 15           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
+        GPIO.setmode(GPIO.BCM)  # Set's GPIO pins to BCM GPIO numbering
+        INPUT_PIN = 15  # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin, so I avoid using it for sensors/buttons
         GPIO.setup(INPUT_PIN, GPIO.IN)
     except:
-        print("was not able to initiallize GPIO Input")
+        print("Was not able to initialize GPIO Input")
         exit(-1)
 
-    while not rospy.is_shutdown():
-        if (GPIO.input(INPUT_PIN) == True): # Physically read the pin now
-            print(colored(255, 0, 0, '################# LEAKAGE DETECTED WATER IN THE BOAT #################'))
-            msg = leakageDetection()
-            msg.header.stamp = rospy.Time.now()
-            msg.leakageDetected = True
-            publisher_leakageStatus.publish(msg)
-        else:
-            msg = leakageDetection()
-            msg.header.stamp = rospy.Time.now()
-            msg.leakageDetected = False
-            publisher_leakageStatus.publish(msg)
-        rate.sleep()
+    rclpy.spin(leak_publisher)
+    leak_publisher.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
